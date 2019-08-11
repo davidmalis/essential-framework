0c28f6afb774fc8f7b259c3c98bbbb58387c6cee
David Mališ <davidmalis.mail@gmail.com>
2019-08-12 00:18:37 +0200

    More testing

--- testapp/src/main/java/org/essentialframework/testapp/beans/CommonController.java
+++ testapp/src/main/java/org/essentialframework/testapp/beans/CommonController.java
@@ -2,6 +2,7 @@ package org.essentialframework.testapp.beans;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.essentialframework.core.annotation.Controller;
@@ -11,21 +12,31 @@ import org.essentialframework.web.annotation.RequestParameter;
 
 @Controller
 public class CommonController{
-
 	
 	@RequestHandler(method = HttpMethod.GET, url = "/")
-	public List<User> test( @RequestParameter("name") String name ) {
+	public List<Person> getPerson( 
+			@RequestParameter("name") Optional<String> name,
+			@RequestParameter("surname") String surname) {
 		return createUserList().stream()
-				.filter(u -> u.getName().equalsIgnoreCase(name))
+				.filter(u -> {
+					
+					if(name.isPresent()) {
+						return name.get().equalsIgnoreCase(u.getName()) && 
+								surname.equalsIgnoreCase(u.getSurname());
+					} else {
+						return surname.equalsIgnoreCase(u.getSurname());
+					}
+					
+				})
 				.collect(Collectors.toList());
 	}
 	
-	List<User> createUserList(){
-		List<User> users = new ArrayList<>();
-		users.add(new User("Ivan"));
-		users.add(new User("David"));
-		users.add(new User("Mario"));
-		users.add(new User("Petar"));
+	List<Person> createUserList(){
+		List<Person> users = new ArrayList<>();
+		users.add(new Person("Ivan", "Horvat", 30));
+		users.add(new Person("Petar", "Perić", 25));
+		users.add(new Person("Ivo", "Ivić", 27));
+		users.add(new Person("Ana", "Anić", 25));
 		return users;
 	}
 	
